4eb26247e35a1825ca1e597b4e7a9a4ac9a78f8c
vitkazak <31851694+vitkazak@users.noreply.github.com>
2019-10-02 18:35:53 +0300

    Cозданы примитивы

---
+++ Graf4/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Graf4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioDot = new System.Windows.Forms.RadioButton();
+            this.radioEdge = new System.Windows.Forms.RadioButton();
+            this.radioSquare = new System.Windows.Forms.RadioButton();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(558, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // radioDot
+            // 
+            this.radioDot.AutoSize = true;
+            this.radioDot.Location = new System.Drawing.Point(636, 44);
+            this.radioDot.Name = "radioDot";
+            this.radioDot.Size = new System.Drawing.Size(69, 21);
+            this.radioDot.TabIndex = 1;
+            this.radioDot.TabStop = true;
+            this.radioDot.Text = "Точка";
+            this.radioDot.UseVisualStyleBackColor = true;
+            // 
+            // radioEdge
+            // 
+            this.radioEdge.AutoSize = true;
+            this.radioEdge.Location = new System.Drawing.Point(636, 72);
+            this.radioEdge.Name = "radioEdge";
+            this.radioEdge.Size = new System.Drawing.Size(85, 21);
+            this.radioEdge.TabIndex = 2;
+            this.radioEdge.TabStop = true;
+            this.radioEdge.Text = "Отрезок";
+            this.radioEdge.UseVisualStyleBackColor = true;
+            // 
+            // radioSquare
+            // 
+            this.radioSquare.AutoSize = true;
+            this.radioSquare.Location = new System.Drawing.Point(636, 100);
+            this.radioSquare.Name = "radioSquare";
+            this.radioSquare.Size = new System.Drawing.Size(84, 21);
+            this.radioSquare.TabIndex = 3;
+            this.radioSquare.TabStop = true;
+            this.radioSquare.Text = "Квадрат";
+            this.radioSquare.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(636, 407);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(114, 30);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "Очистить";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(590, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(688, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(688, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.radioSquare);
+            this.Controls.Add(this.radioEdge);
+            this.Controls.Add(this.radioDot);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioDot;
+        private System.Windows.Forms.RadioButton radioEdge;
+        private System.Windows.Forms.RadioButton radioSquare;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Graf4/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graf4
+{
+    public partial class Form1 : Form
+    {
+        private Image CorrectImage;
+        Graphics g;
+        Pen blackPen = new Pen(Color.Black, 2);
+        private Color CorrectColor = Color.Black;
+        private Point prevLocation;
+        bool isDown = true;
+        float size = 1;
+        bool PrevExist = false;
+        Point PrevPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            CorrectImage = DrawingImageByColor(new Bitmap(pictureBox1.Width, pictureBox1.Height), Color.White);
+            pictureBox1.Image = CorrectImage;
+            g = Graphics.FromImage(CorrectImage);
+
+        }
+
+        private Bitmap DrawingImageByColor(Image Image, Color color)
+        {
+            Bitmap res = new Bitmap(Image);
+            for (int x = 0; x < res.Width; x++)
+            {
+                for (int y = 0; y < res.Height; y++)
+                {
+                    res.SetPixel(x, y, color);
+                }
+            }
+            return res;
+        }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioDot.Checked)
+            {
+                PrevExist = false;
+                Pen pen = new Pen(CorrectColor);
+                SolidBrush solid = new SolidBrush(CorrectColor);
+                g.FillEllipse(solid, e.X, e.Y, 5, 5);
+                g.DrawEllipse(pen, e.X, e.Y, 5, 5);
+                pictureBox1.Refresh();
+
+                solid.Dispose();
+                pen.Dispose();
+            }
+            else if(radioEdge.Checked && !isDown)
+            {
+                if (g != null)
+                {
+                    g.Dispose();
+                }
+                PrevExist = false;
+                g = Graphics.FromImage(CorrectImage);
+                g.DrawLine(new Pen(CorrectColor, 4), prevLocation, prevLocation = new Point(e.X, e.Y));
+                pictureBox1.Image = CorrectImage;
+            }
+            else if(radioSquare.Checked && !isDown)
+            {
+                if (g != null)
+                {
+                    g.Dispose();
+                }
+                //DrawRectangle(sender,e);
+                //prevLocation = new Point(e.X, e.Y);
+                g = Graphics.FromImage(CorrectImage);
+                //g.DrawRectangle(new Pen(CorrectColor, 4), prevLocation.X, prevLocation.Y, 60,60);
+                //pictureBox1.Image = CorrectImage;
+                if (PrevExist)
+                {
+                    PrevExist = false;
+                    g.DrawRectangle(new Pen(CorrectColor, 4), PrevPoint.X, PrevPoint.Y, PrevPoint.Y - e.Y, PrevPoint.Y - e.Y);
+                    pictureBox1.Image = CorrectImage;
+                }
+                else
+                {
+                    PrevExist = true;
+                    PrevPoint = new Point(e.X, e.Y);
+                }
+            }
+        }
+
+        private int[] PicMove(int x, int y, int dx, int dy)
+        {
+            int[,] matr = new int[3,3]{ { 1, 0, 0 },{ 0, 1, 0 },{ dx*(-1), dy*(-1), 1 } };
+            int[] coord = new int[]{ x, y, 1 };
+            int[] res = new int[3] {0,0,0};
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    res[i] += matr[j,i] * coord[j];
+            return res;
+        }
+
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            for (int y = 0; y < img.Height-1; y++)
+                for (int x = 0; x < img.Width-1; x++)
+                {
+                    if (img.GetPixel(x, y).Equals(Color.FromArgb(0,0,0))){
+                        int[] res = PicMove(x, y, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                        img.SetPixel(x, y, Color.White);
+                        img.SetPixel(res[0], res[1], Color.Black);
+                    }
+                }
+            pictureBox1.Image = img;
+        }
+
+        //------------------------------------------------------------------------------------------
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isDown = false;
+            prevLocation = e.Location;
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isDown = true;
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            if (CorrectImage != null)
+            {
+                CorrectImage.Dispose();
+            }
+            CorrectImage = DrawingImageByColor(new Bitmap(pictureBox1.Width, pictureBox1.Height), Color.White);
+            if (g != null)
+            {
+                g.Dispose();
+            }
+            g = Graphics.FromImage(CorrectImage);
+            pictureBox1.Image = CorrectImage;
+        }
+    }
+}
