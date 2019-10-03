1dfde39a3cc644b31127eb0b677b64ab69f26771
Nekdil01 <36044753+Nekdil01@users.noreply.github.com>
2019-10-04 00:46:12 +0300

    Draw, Inters, Inside
    Добавлены отрисовка примитивов, нахождение пересечений, определение принадлежности точки полигону
    
    TODO: аффинные преобразования

--- Graf4/Form1.Designer.cs
+++ Graf4/Form1.Designer.cs
@@ -33,29 +33,31 @@
             this.radioEdge = new System.Windows.Forms.RadioButton();
             this.radioSquare = new System.Windows.Forms.RadioButton();
             this.buttonReset = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.intersection = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(558, 426);
+            this.pictureBox1.Size = new System.Drawing.Size(677, 520);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
             // 
             // radioDot
             // 
             this.radioDot.AutoSize = true;
-            this.radioDot.Location = new System.Drawing.Point(636, 44);
+            this.radioDot.Location = new System.Drawing.Point(773, 22);
+            this.radioDot.Margin = new System.Windows.Forms.Padding(2);
             this.radioDot.Name = "radioDot";
-            this.radioDot.Size = new System.Drawing.Size(69, 21);
+            this.radioDot.Size = new System.Drawing.Size(55, 17);
             this.radioDot.TabIndex = 1;
             this.radioDot.TabStop = true;
             this.radioDot.Text = "Точка";
@@ -64,9 +66,10 @@
             // radioEdge
             // 
             this.radioEdge.AutoSize = true;
-            this.radioEdge.Location = new System.Drawing.Point(636, 72);
+            this.radioEdge.Location = new System.Drawing.Point(773, 44);
+            this.radioEdge.Margin = new System.Windows.Forms.Padding(2);
             this.radioEdge.Name = "radioEdge";
-            this.radioEdge.Size = new System.Drawing.Size(85, 21);
+            this.radioEdge.Size = new System.Drawing.Size(68, 17);
             this.radioEdge.TabIndex = 2;
             this.radioEdge.TabStop = true;
             this.radioEdge.Text = "Отрезок";
@@ -75,61 +78,57 @@
             // radioSquare
             // 
             this.radioSquare.AutoSize = true;
-            this.radioSquare.Location = new System.Drawing.Point(636, 100);
+            this.radioSquare.Location = new System.Drawing.Point(773, 67);
+            this.radioSquare.Margin = new System.Windows.Forms.Padding(2);
             this.radioSquare.Name = "radioSquare";
-            this.radioSquare.Size = new System.Drawing.Size(84, 21);
+            this.radioSquare.Size = new System.Drawing.Size(68, 17);
             this.radioSquare.TabIndex = 3;
             this.radioSquare.TabStop = true;
-            this.radioSquare.Text = "Квадрат";
+            this.radioSquare.Text = "Полигон";
             this.radioSquare.UseVisualStyleBackColor = true;
             // 
             // buttonReset
             // 
-            this.buttonReset.Location = new System.Drawing.Point(636, 407);
+            this.buttonReset.Location = new System.Drawing.Point(773, 510);
+            this.buttonReset.Margin = new System.Windows.Forms.Padding(2);
             this.buttonReset.Name = "buttonReset";
-            this.buttonReset.Size = new System.Drawing.Size(114, 30);
+            this.buttonReset.Size = new System.Drawing.Size(86, 24);
             this.buttonReset.TabIndex = 4;
             this.buttonReset.Text = "Очистить";
             this.buttonReset.UseVisualStyleBackColor = true;
             this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
             // 
-            // button1
+            // intersection
             // 
-            this.button1.Location = new System.Drawing.Point(590, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.intersection.Location = new System.Drawing.Point(773, 482);
+            this.intersection.Name = "intersection";
+            this.intersection.Size = new System.Drawing.Size(86, 23);
+            this.intersection.TabIndex = 5;
+            this.intersection.Text = "Intersection";
+            this.intersection.UseVisualStyleBackColor = true;
+            this.intersection.Click += new System.EventHandler(this.Intersection_Click);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(688, 165);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(56, 22);
-            this.textBox1.TabIndex = 6;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(688, 193);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(56, 22);
-            this.textBox2.TabIndex = 7;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(927, 541);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.intersection);
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.radioSquare);
             this.Controls.Add(this.radioEdge);
             this.Controls.Add(this.radioDot);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -145,9 +144,8 @@
         private System.Windows.Forms.RadioButton radioEdge;
         private System.Windows.Forms.RadioButton radioSquare;
         private System.Windows.Forms.Button buttonReset;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button intersection;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Graf4/Form1.cs
+++ Graf4/Form1.cs
@@ -12,143 +12,202 @@ namespace Graf4
 {
     public partial class Form1 : Form
     {
-        private Image CorrectImage;
         Graphics g;
-        Pen blackPen = new Pen(Color.Black, 2);
-        private Color CorrectColor = Color.Black;
-        private Point prevLocation;
-        bool isDown = true;
-        float size = 1;
-        bool PrevExist = false;
-        Point PrevPoint;
+        Point click;
+        Point strt, end, minpol, maxpol;
+        Point[] line = new Point[2];
+        Point[] pol = new Point[0];
+
         public Form1()
         {
             InitializeComponent();
-            CorrectImage = DrawingImageByColor(new Bitmap(pictureBox1.Width, pictureBox1.Height), Color.White);
-            pictureBox1.Image = CorrectImage;
-            g = Graphics.FromImage(CorrectImage);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
 
         }
 
-        private Bitmap DrawingImageByColor(Image Image, Color color)
-        {
-            Bitmap res = new Bitmap(Image);
-            for (int x = 0; x < res.Width; x++)
-            {
-                for (int y = 0; y < res.Height; y++)
-                {
-                    res.SetPixel(x, y, color);
-                }
-            }
-            return res;
-        }
-
-        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
-        {
-            if (radioDot.Checked)
-            {
-                PrevExist = false;
-                Pen pen = new Pen(CorrectColor);
-                SolidBrush solid = new SolidBrush(CorrectColor);
-                g.FillEllipse(solid, e.X, e.Y, 5, 5);
-                g.DrawEllipse(pen, e.X, e.Y, 5, 5);
-                pictureBox1.Refresh();
-
-                solid.Dispose();
-                pen.Dispose();
-            }
-            else if(radioEdge.Checked && !isDown)
-            {
-                if (g != null)
-                {
-                    g.Dispose();
-                }
-                PrevExist = false;
-                g = Graphics.FromImage(CorrectImage);
-                g.DrawLine(new Pen(CorrectColor, 4), prevLocation, prevLocation = new Point(e.X, e.Y));
-                pictureBox1.Image = CorrectImage;
-            }
-            else if(radioSquare.Checked && !isDown)
-            {
-                if (g != null)
-                {
-                    g.Dispose();
-                }
-                //DrawRectangle(sender,e);
-                //prevLocation = new Point(e.X, e.Y);
-                g = Graphics.FromImage(CorrectImage);
-                //g.DrawRectangle(new Pen(CorrectColor, 4), prevLocation.X, prevLocation.Y, 60,60);
-                //pictureBox1.Image = CorrectImage;
-                if (PrevExist)
-                {
-                    PrevExist = false;
-                    g.DrawRectangle(new Pen(CorrectColor, 4), PrevPoint.X, PrevPoint.Y, PrevPoint.Y - e.Y, PrevPoint.Y - e.Y);
-                    pictureBox1.Image = CorrectImage;
-                }
-                else
-                {
-                    PrevExist = true;
-                    PrevPoint = new Point(e.X, e.Y);
-                }
-            }
-        }
-
-        private int[] PicMove(int x, int y, int dx, int dy)
-        {
-            int[,] matr = new int[3,3]{ { 1, 0, 0 },{ 0, 1, 0 },{ dx*(-1), dy*(-1), 1 } };
-            int[] coord = new int[]{ x, y, 1 };
-            int[] res = new int[3] {0,0,0};
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    res[i] += matr[j,i] * coord[j];
-            return res;
-        }
-
-
-
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            Bitmap img = new Bitmap(pictureBox1.Image);
-            for (int y = 0; y < img.Height-1; y++)
-                for (int x = 0; x < img.Width-1; x++)
-                {
-                    if (img.GetPixel(x, y).Equals(Color.FromArgb(0,0,0))){
-                        int[] res = PicMove(x, y, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
-                        img.SetPixel(x, y, Color.White);
-                        img.SetPixel(res[0], res[1], Color.Black);
-                    }
-                }
-            pictureBox1.Image = img;
-        }
-
-        //------------------------------------------------------------------------------------------
         private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
-                isDown = false;
-            prevLocation = e.Location;
+                if (radioEdge.Checked)
+                {
+                    strt = e.Location;
+                }
+                else if (radioSquare.Checked && pol.Length == 0)
+                {
+                    strt = e.Location;
+                    Array.Resize(ref pol, 1);
+                    pol[pol.Length - 1] = strt;
+                }
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                if (radioEdge.Checked)
+                {
+                    end = e.Location;
+                    pictureBox1.Invalidate();
+                }
+                else if (radioSquare.Checked)
+                {
+                    end = e.Location;
+                    pictureBox1.Invalidate();
+                }
+        }
+
+        private void Intersection_Click(object sender, EventArgs e)
+        {
+            PointF intersection = new PointF();
+            PointF[] intersections = new PointF[0];
+            if (line.Length > 4)
+            {
+                for (int i = 1; i <= line.Length; i += 2)
+                {
+                    for (int j = i + 2; j <= line.Length; j += 2)
+                    {
+                        intersection = findIntersection(line[i - 1], line[i], line[j - 1], line[j]);
+                        g.DrawEllipse(Pens.Red, intersection.X-3, intersection.Y-3, 6, 6);
+                        pictureBox1.Invalidate();
+                    }
+                }
+            }
+
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Invalidate();
+            Array.Clear(line, 0, line.Length);
+            Array.Clear(pol, 0, pol.Length);
+            Array.Resize(ref line, 2);
+            Array.Resize(ref pol, 0);
+            click.X = 0; click.Y = 0;
+            strt = end = Point.Empty;
         }
 
         private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
-                isDown = true;
+                if (radioEdge.Checked)
+                {
+                    line[line.Length - 2] = strt;
+                    line[line.Length - 1] = end;
+                    Array.Resize(ref line, line.Length + 2);
+
+                }
+                else if (radioSquare.Checked)
+                {
+                    Array.Resize(ref pol, pol.Length + 1);
+                    pol[pol.Length - 1] = end;
+                    if (end.X < minpol.X)
+                        minpol.X = end.X;
+                    if (end.Y < minpol.Y)
+                        minpol.Y = end.Y;
+                    if (end.X > maxpol.X)
+                        maxpol.X = end.X;
+                    if (end.Y > maxpol.Y)
+                        maxpol.Y = end.Y;
+                    strt = end;
+                }
+                else if (radioDot.Checked)
+                {
+                    click = e.Location;
+                }
+            pictureBox1.Invalidate();
         }
 
-        private void ButtonReset_Click(object sender, EventArgs e)
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            if (CorrectImage != null)
+            for (int i = 0; i < line.Length; i += 2)
             {
-                CorrectImage.Dispose();
+                e.Graphics.DrawLine(Pens.Black, line[i], line[i + 1]);
             }
-            CorrectImage = DrawingImageByColor(new Bitmap(pictureBox1.Width, pictureBox1.Height), Color.White);
-            if (g != null)
+            if (pol.Length > 1)
+                e.Graphics.DrawPolygon(Pens.Black, pol);
+            e.Graphics.DrawLine(Pens.Black, strt, end);
+            if (radioDot.Checked)
+                e.Graphics.DrawEllipse(Pens.Black,click.X,click.Y,4,4);
+            label_info();
+        }
+
+        PointF findIntersection(PointF p0, PointF p1, PointF p2, PointF p3)
+        {
+            PointF i = new PointF(-1, -1);
+            PointF s1 = new PointF();
+            PointF s2 = new PointF();
+            s1.X = p1.X - p0.X; s1.Y = p1.Y - p0.Y;
+            s2.X = p3.X - p2.X; s2.Y = p3.Y - p2.Y;
+            float s, t;
+            s = (-s1.Y * (p0.X - p2.X) + s1.X * (p0.Y - p2.Y)) / (-s2.X * s1.Y + s1.X * s2.Y);
+            t = (s2.X * (p0.Y - p2.Y) - s2.Y * (p0.X - p2.X)) / (-s2.X * s1.Y + s1.X * s2.Y);
+
+            if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
             {
-                g.Dispose();
+                i.X = p0.X + (t * s1.X);
+                i.Y = p0.Y + (t * s1.Y);
+
             }
-            g = Graphics.FromImage(CorrectImage);
-            pictureBox1.Image = CorrectImage;
+            return i;
+        }
+
+        bool isInside(Point[] polygon, PointF p)
+        {
+            int n = polygon.Length;
+            if (n < 3) return false;
+            PointF extreme = new PointF(pictureBox1.Width, p.Y);
+            int count = 0, i = 0;
+            do
+            {
+                int next = (i + 1) % n;
+                PointF intersection = findIntersection(polygon[i], polygon[next], p, extreme);
+                if (intersection.X != -1)
+                {
+                    if (orientation(polygon[i], p, polygon[next]) == 0)
+                        return onSegment(polygon[i], p, polygon[next]);
+                    count++;
+                }
+                i = next;
+            } while (i != 0);
+            return count % 2 == 1;
+        }
+
+        int orientation(PointF p, PointF q, PointF r)
+        {
+            float val = (q.Y - p.Y) * (r.X - q.X) -
+                      (q.X - p.X) * (r.Y - q.Y);
+
+            if (val == 0) return 0;
+            return (val > 0) ? 1 : 2;
+        }
+
+        bool onSegment(PointF q, PointF p, PointF r)
+        {
+            if (q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) &&
+                    q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y))
+                return true;
+            return false;
+        }
+
+        private void label_info()
+        {
+            if (pol.Length > 2)
+            {
+                if (isInside(pol, click))
+                {
+                    label1.Text = "Принадлежит многоугольнику";
+                }
+                else
+                {
+                    label1.Text = "Не принадлежит многоугольнику";
+                }
+            }
+            else
+            {
+                label1.Text = "";
+            }
         }
     }
 }
